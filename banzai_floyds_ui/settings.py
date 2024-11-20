966b85d071bf49452c5a5d87fb89b85d9317e0ec
Curtis McCully <cmccully@lco.global>
2024-11-20 16:18:09 -0500

    Updated localhost to 127.0.0.1 and added http to make django happy.

--- banzai_floyds_ui/settings.py
+++ banzai_floyds_ui/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'localhost').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1').split(',')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
